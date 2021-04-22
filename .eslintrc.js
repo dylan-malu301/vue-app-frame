271df3c0d824475fa63dd6ce2d0356a6e0a1a668
马璐 <58161885+dylan-malu301@users.noreply.github.com>
2021-04-22 16:10:02 +0800

    Add files via upload

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    root: true,
+    env: {
+      node: true
+    },
+    'extends': [
+      'plugin:vue/essential',
+      '@vue/standard'
+    ],
+    rules: {
+      'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    },
+    parserOptions: {
+      parser: 'babel-eslint'
+    },
+    globals: {
+      AMap: true  
+    },
+    overrides: [
+      {
+        files: [
+          '**/__tests__/*.{j,t}s?(x)'
+        ],
+        env: {
+          mocha: true
+        }
+      }
+    ]
+  }
+  
